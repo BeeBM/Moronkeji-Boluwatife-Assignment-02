8686a4f57f8730e24e4d109527f03b35195fd606
Boluwatife Moronkeji <104370434+BeeBM@users.noreply.github.com>
2022-07-06 08:50:30 +0000

    Implemented the functions for Challenges 1 and 3 respectively.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for(i = 0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i]
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -47,9 +51,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let result = []
+  for (i = 0; i < arrayOfNumbers.length; i++){
+    let fahrenValue = Math.trunc((arrayOfNumbers[i] * 9/5) + 32)
+    result.push(fahrenValue);
+  }
+    return result;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
